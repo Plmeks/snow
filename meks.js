427c7604d66fecab33c39cbc6fdec4e627d832f4
Plmeks <plmeks94@gmail.com>
2017-12-29 22:35:56 +0300

    Snow

--- meks.js
+++ meks.js
@@ -1,4 +1,22 @@
 
 var meks = function() {
-	console.log('here!');
+	console.log('here');
+	$('body').flurry({
+	  character: "❄",
+	  color: "white",
+	  frequency: 100,
+	  speed: 3000,
+	  small: 8,
+	  large: 28,
+	  wind: 40,
+	  windVariance: 20,
+	  rotation: 90,
+	  rotationVariance: 180,
+	  startOpacity: 1,
+	  endOpacity: 0,
+	  opacityEasing: "cubic-bezier(1,.3,.6,.74)",
+	  blur: true,
+	  overflow: "hidden",
+	  zIndex: 9999
+	});
 };
