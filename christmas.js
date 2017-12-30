8b4e237fa5ebe2fc5315858d9e3f117957886e68
Plmeks <plmeks94@gmail.com>
2017-12-30 14:00:51 +0300

    Snow

--- christmas.js
+++ christmas.js
@@ -13,31 +13,40 @@ document.body.insertAdjacentHTML('beforeend', `
    <div id="snow" style="z-index: 9999; pointer-events: none; position: fixed; top: 0; left: 0;"></div>
 `);
 
-setTimeout(function() {
-	$("#snow").height($(window).innerHeight());
-	$("#snow").width($(window).innerWidth());
-	
-	(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)){ return; }
-    js = d.createElement(s); js.id = id;
-    js.onload = function(){
-        // remote script has loaded
-    };
-    js.src = "https://cdn.rawgit.com/Plmeks/snow/6f58ac7e/jquery.flurry.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'www'));
-
-	setTimeout(function() {
-		$('#snow').flurry({
-		character: "❄",
-		height: $(window).innerHeight() + 200,
-		speed: 11000,
-		small: 10,
-		large: 68
-	});
-	}, 500)
-}, 500);
+(function touchJquery () {
+	if (!$) {
+		window.requestAnimationFrame(touchJquery);
+	} else {
+		$("#snow").height($(window).innerHeight());
+		$("#snow").width($(window).innerWidth());
+		
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)){ return; }
+			js = d.createElement(s); js.id = id;
+			js.onload = function(){
+				// remote script has loaded
+			};
+			js.src = "https://cdn.rawgit.com/Plmeks/snow/6f58ac7e/jquery.flurry.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'www'));
+		
+		(function touchFlurry() {
+			if(!$().flurry) {
+				window.requestAnimationFrame(touchFlurry);
+			} else {
+				$('#snow').flurry({
+					character: "❄",
+					height: $(window).innerHeight() + 200,
+					speed: 11000,
+					small: 10,
+					large: 68
+				});
+			}
+			
+		})();
+	}
+})();
 
 
 
